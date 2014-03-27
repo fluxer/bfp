e83ec3f614890f5acb2cf33496d9a152e3608d71
Ivailo Monev <xakepa10@gmail.com>
2014-03-27 00:56:00 +0000

    added mounttray and stripped powertray

--- src/powertray/powertray.py
+++ src/powertray/powertray.py
@@ -13,28 +13,8 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
         self.systembus = QtDBus.QDBusConnection.systemBus()
         self.iface = QtDBus.QDBusInterface('org.powerd.PowerD', '/org/powerd/PowerD',
             'org.powerd.Interface', self.systembus)
-        self.systembus.connect('org.powerd.PowerD', '/org/powerd/PowerD',
-            'org.powerd.Interface', 'NotifyMount', self.AddDevice)
-        self.systembus.connect('org.powerd.PowerD', '/org/powerd/PowerD',
-            'org.powerd.Interface', 'NotifyUnmount', self.RemoveDevice)
-        self.GenerateMenu()
 
-    def GenerateMenu(self):
-        ''' Generate tray menu based on devices available '''
         self.menu = QtGui.QMenu(self.parent)
-
-        for dev in os.listdir('/dev/disk/by-uuid'):
-            device = os.path.join('/dev/disk/by-uuid', dev)
-            if os.path.ismount('/media/' + dev):
-                entry = self.menu.addAction(QtGui.QIcon.fromTheme('media-eject'), dev)
-                self.connect(entry, QtCore.SIGNAL('triggered()'),
-                    lambda device=device: self.UnmountDevice(device))
-            else:
-                entry = self.menu.addAction(QtGui.QIcon.fromTheme('drive-removable-media'), dev)
-                self.connect(entry, QtCore.SIGNAL('triggered()'),
-                    lambda device=device: self.MountDevice(device))
-
-        self.menu.addSeparator()
         self.menu.addAction(QtGui.QIcon.fromTheme('uninterruptible-power-supply'), 'Suspend', self.Suspend)
         self.menu.addAction(QtGui.QIcon.fromTheme('system-shutdown'), 'Shutdown', self.Shutdown)
         self.menu.addAction(QtGui.QIcon.fromTheme('view-refresh'), 'Reboot', self.Reboot)
@@ -83,37 +63,6 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
         reply = QtDBus.QDBusReply(msg)
         if not reply.isValid():
             self.ErrorMessage(reply.error().message())
-
-    @QtCore.pyqtSlot(str)
-    def AddDevice(self, device):
-        ''' Add device to the menu, called upon power daemon notification '''
-        self.GenerateMenu()
-        self.BrowseDevice(str(device))
-
-    @QtCore.pyqtSlot(str)
-    def RemoveDevice(self, device):
-        ''' Remove device from the menu, called upon power daemon notification '''
-        self.GenerateMenu()
-
-    def BrowseDevice(self, device):
-        ''' Browse mounted device with file-manager '''
-        os.system('xdg-open /media/' + os.path.basename(device))
-
-    def MountDevice(self, device):
-        ''' Mount device and regenerate menu '''
-        msg = self.iface.call('Mount', device)
-        reply = QtDBus.QDBusReply(msg)
-        if not reply.isValid():
-            self.ErrorMessage(reply.error().message())
-        self.GenerateMenu()
-
-    def UnmountDevice(self, device):
-        ''' Unmount device and regenerate menu '''
-        msg = self.iface.call('Unmount', device)
-        reply = QtDBus.QDBusReply(msg)
-        if not reply.isValid():
-            self.ErrorMessage(reply.error().message())
-        self.GenerateMenu()
 
 app = QtGui.QApplication(sys.argv)
 style = app.style()
