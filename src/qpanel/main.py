7c25bfdcfb8a706465f60dc4a476800499e66291
Ivailo Monev <xakepa10@gmail.com>
2014-06-22 10:58:31 +0000

    added qpanel

---
+++ src/qpanel/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import qpanel_ui
+from PyQt4 import QtCore, QtGui
+import sys, os
+import libmisc
+misc = libmisc.Misc()
+import libqdesktop
+
+# prepare for lift-off
+app = QtGui.QApplication(sys.argv)
+MainWindow = QtGui.QMainWindow()
+ui = qpanel_ui.Ui_MainWindow()
+ui.setupUi(MainWindow)
+
+config = libqdesktop.Config()
+icon = QtGui.QIcon()
+
+def run_preferences():
+    p = QtCore.QProcess()
+    p.setWorkingDirectory(QtCore.QDir.homePath())
+    p.startDetached('qsettings')
+    p.close()
+
+def setLook():
+    config.read()
+    ssheet = '/etc/qdesktop/styles/' + config.GENERAL_STYLESHEET + '/style.qss'
+    if config.GENERAL_STYLESHEET and os.path.isfile(ssheet):
+        app.setStyleSheet(misc.file_read(ssheet))
+    else:
+        app.setStyleSheet('')
+    icon.setThemeName(config.GENERAL_ICONTHEME)
+setLook()
+
+def show_popup():
+    ui.menuActions.popup(QtGui.QCursor.pos())
+
+ui.actionPreferences.triggered.connect(run_preferences)
+ui.actionLogout.triggered.connect(sys.exit)
+
+ui.menubar.hide()
+ui.MenuButton.clicked.connect(show_popup)
+
+# create dynamic menu
+menu = libqdesktop.Menu(app, ui.menuApplications)
+menu.build()
+
+# setup window
+desktop = QtGui.QDesktopWidget()
+MainWindow.resize(desktop.width(), 50)
+MainWindow.move(0, desktop.rect().bottom() - 50)
+MainWindow.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.Tool)
+
+# run!
+MainWindow.show()
+sys.exit(app.exec_())
+
