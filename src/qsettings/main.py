b28b7f581844c22813f8a0049db7b24fc76211f4
Ivailo Monev <xakepa10@gmail.com>
2014-06-29 03:24:08 +0000

    version bump

--- src/qsettings/main.py
+++ src/qsettings/main.py
@@ -170,7 +170,7 @@ for svar in misc.list_dirs('/etc/qdesktop/styles'):
     if os.path.isfile(svar + '/style.qss'):
         ui.StyleBox.addItem(os.path.basename(svar))
 
-for svar in misc.list_dirs('/share/icons'):
+for svar in misc.list_dirs(sys.prefix + '/share/icons'):
     if os.path.isfile(svar + '/index.theme'):
         ui.IconThemeBox.addItem(os.path.basename(svar))
 
