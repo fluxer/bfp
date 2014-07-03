58feaccb5f152655ddfa2d9bdaa76471d6927648
Ivailo Monev <xakepa10@gmail.com>
2014-07-03 03:22:57 +0000

    keyboard shortcuts added

--- src/qscreenshot/main.py
+++ src/qscreenshot/main.py
@@ -5,7 +5,7 @@ from PyQt4 import QtGui
 import sys, time, libmisc, libdesktop
 
 # prepare for lift-off
-app_version = "0.9.4 (9bf058a)"
+app_version = "0.9.4 (7516e29)"
 app = QtGui.QApplication(sys.argv)
 MainWindow = QtGui.QMainWindow()
 ui = qscreenshot_ui.Ui_MainWindow()
