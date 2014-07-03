733a716a13231ffbddc3eb98b9485c62225c98bf
Ivailo Monev <xakepa10@gmail.com>
2014-07-03 01:06:32 +0000

    added keyboard shortcuts fpr qfile and qedit actions

--- src/qedit/main.py
+++ src/qedit/main.py
@@ -5,7 +5,7 @@ from PyQt4 import QtCore, QtGui
 import sys, os, libmisc, libdesktop, libhighlighter
 
 # prepare for lift-off
-app_version = "0.9.4 (9bf058a)"
+app_version = "0.9.4 (b0aee36)"
 app = QtGui.QApplication(sys.argv)
 MainWindow = QtGui.QMainWindow()
 ui = qedit_ui.Ui_MainWindow()
