0dc431dada3727faebefb54ce6f58d1826253f59
Ivailo Monev <xakepa10@gmail.com>
2014-07-03 01:11:43 +0000

    keyboard shortcuts changed and added

--- src/qbrowse/main.py
+++ src/qbrowse/main.py
@@ -5,7 +5,7 @@ from PyQt4 import QtCore, QtGui
 import sys, libmisc, libdesktop
 
 # prepare for lift-off
-app_version = "0.9.3 (9bf058a)"
+app_version = "0.9.4 (733a716)"
 app = QtGui.QApplication(sys.argv)
 MainWindow = QtGui.QMainWindow()
 ui = qbrowse_ui.Ui_MainWindow()
