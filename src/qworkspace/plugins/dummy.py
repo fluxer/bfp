179dbfdeed547378a120cf7c4ece5928abb20d17
Ivailo Monev <xakepa10@gmail.com>
2014-07-16 19:29:49 +0000

    updated dummy plugin

--- src/qworkspace/plugins/dummy.py
+++ src/qworkspace/plugins/dummy.py
@@ -8,10 +8,11 @@ class Widget(QtGui.QWidget):
     ''' Tab widget '''
     def __init__(self, parent, spath=None):
         super(Widget, self).__init__(parent)
-        self.name = 'editor'
+        self.name = 'dummy'
 
 
 class Plugin(object):
+    ''' Plugin handler '''
     def __init__(self, parent=None):
         self.name = 'dummy'
         self.version = '0.0.1'
@@ -19,10 +20,18 @@ class Plugin(object):
         self.icon = general.get_icon('delete')
 
     def open(self, spath):
-        pass
+        ''' Open path in new tab '''
+        self.index = self.parent.tabWidget.currentIndex()+1
+        self.parent.tabWidget.insertTab(self.index, Widget(self.parent, spath), 'Storage')
+        self.parent.tabWidget.setCurrentIndex(self.index)
+        self.widget = self.parent.tabWidget.widget(self.index)
 
     def close(self):
-        pass
+        ''' Close tab '''
+        if self.widget:
+            self.widget.deleteLater()
+            self.parent.tabWidget.removeTab(self.index)
 
     def unload(self):
-        pass
+        ''' Unload plugin '''
+        self.close()
