783d0e8418bb8d3203d14c45f86037568ffdaee8
Ivailo Monev <xakepa10@gmail.com>
2014-08-03 09:39:33 +0000

    update translations for all plugins

--- src/qworkspace/main.py
+++ src/qworkspace/main.py
@@ -5,7 +5,7 @@ from PyQt4 import QtCore, QtGui
 import sys, os, gc, libworkspace, libmisc
 
 # prepare for lift-off
-app_version = "0.9.20 (d0e8c83)"
+app_version = "0.9.20 (c9f137d)"
 app = QtGui.QApplication(sys.argv)
 MainWindow = QtGui.QMainWindow()
 ui = qworkspace_ui.Ui_MainWindow()
--- src/qworkspace/tr/qworkspace_bg.ts
+++ src/qworkspace/tr/qworkspace_bg.ts
@@ -12,6 +12,81 @@
         <source>Editor</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <location filename="dummy.py" line="24"/>
+        <source>Dummy plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dummy.py" line="32"/>
+        <source>Dummy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="image.py" line="115"/>
+        <source>Image viewer plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="image.py" line="134"/>
+        <source>Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="multimedia.py" line="66"/>
+        <source>Media plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="multimedia.py" line="90"/>
+        <source>Multimedia</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="quit.py" line="14"/>
+        <source>Quit plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.py" line="64"/>
+        <source>Screenshot taking plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.py" line="77"/>
+        <source>Screenshot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="282"/>
+        <source>Storage management plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="304"/>
+        <source>Storage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="terminal.py" line="32"/>
+        <source>Embed terminal plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="terminal.py" line="45"/>
+        <source>Terminal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="349"/>
+        <source>World Wide Web browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="362"/>
+        <source>WWW</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>Widget</name>
@@ -21,27 +96,27 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="editor.py" line="22"/>
+        <location filename="www.py" line="119"/>
         <source>CTRL+S</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="editor.py" line="28"/>
+        <location filename="www.py" line="115"/>
         <source>CTRL+R</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="editor.py" line="32"/>
+        <location filename="www.py" line="117"/>
         <source>CTRL+F</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="editor.py" line="36"/>
+        <location filename="www.py" line="113"/>
         <source>CTRL+N</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="editor.py" line="58"/>
+        <location filename="storage.py" line="202"/>
         <source>Open</source>
         <translation type="unfinished"></translation>
     </message>
@@ -56,8 +131,243 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="editor.py" line="102"/>
+        <location filename="www.py" line="288"/>
         <source>Find</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.py" line="26"/>
+        <source>Image Files (*.png *.jpg *.jpeg *.svg);;All Files (*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="screenshot.py" line="26"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="201"/>
+        <source>Execute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="203"/>
+        <source>Open with</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="205"/>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="206"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="207"/>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="209"/>
+        <source>Rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="210"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="211"/>
+        <source>Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="213"/>
+        <source>New file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="storage.py" line="214"/>
+        <source>New directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="110"/>
+        <source>CTRL+B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="183"/>
+        <source>Untitled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="251"/>
+        <source>the remote server refused the connection (the server is not accepting requests)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="252"/>
+        <source>the remote server closed the connection prematurely, before the entire reply was received and processed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="253"/>
+        <source>the remote host name was not found (invalid hostname)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="254"/>
+        <source>the connection to the remote server timed out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="255"/>
+        <source>the operation was canceled via calls to abort() or close() before it was finished.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="256"/>
+        <source>the SSL/TLS handshake failed and the encrypted channel could not be established. The sslErrors() signal should have been emitted.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="257"/>
+        <source>the connection was broken due to disconnection from the network, however the system has initiated roaming to another acess point. The request should be resubmitted and will be processed as soon as the connection is re-established.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="258"/>
+        <source>the connection to the proxy server was refused (the proxy server is not accepting requests)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="259"/>
+        <source>the proxy server closed the connection prematurely, before the entire reply was received and processed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="260"/>
+        <source>the proxy host name was not found (invalid proxy hostname)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="261"/>
+        <source>the connection to the proxy timed out or the proxy did not reply in time to the request sent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="262"/>
+        <source>the proxy requires authentication in order to honour the request but did not accept any credentials offered (if any)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="263"/>
+        <source>the access to the remote content was denied (similar to HTTP error 401)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="264"/>
+        <source>the operation requested on the remote content is not permitted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="265"/>
+        <source>the remote content was not found at the server (similar to HTTP error 404)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="266"/>
+        <source>the remote server requires authentication to serve the content but the credentials provided were not accepted (if any)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="267"/>
+        <source>the request needed to be sent again, but this failed for example because the upload data could not be read a second time.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="268"/>
+        <source>the Network Access API cannot honor the request because the protocol is not known</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="269"/>
+        <source>the requested operation is invalid for this protocol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="270"/>
+        <source>an unknown network-related error was detected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="271"/>
+        <source>an unknown proxy-related error was detected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="272"/>
+        <source>an unknown error related to the remote content was detected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="273"/>
+        <source>a breakdown in protocol was detected (parsing error, invalid or unexpected responses, etc.)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="276"/>
+        <source>unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="284"/>
+        <source>SSL errors ignored: %s, %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="294"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="316"/>
+        <source>Critical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="316"/>
+        <source>Dowload of &lt;b&gt;%s&lt;/b&gt; failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="319"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="319"/>
+        <source>Dowload of &lt;b&gt;%s&lt;/b&gt; complete.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="335"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="336"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="www.py" line="337"/>
+        <source>Reload</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
