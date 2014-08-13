29df618c15ef9272db19b13bf42f036467cbcdec
Ivailo Monev <xakepa10@gmail.com>
2014-08-13 09:23:44 +0300

    build and install package plugin

--- src/qworkspace/main.py
+++ src/qworkspace/main.py
@@ -5,7 +5,7 @@ from PyQt4 import QtCore, QtGui
 import sys, os, gc, libworkspace, libmisc
 
 # prepare for lift-off
-app_version = "0.9.22 (0acee94)"
+app_version = "0.9.22 (5836145)"
 app = QtGui.QApplication(sys.argv)
 MainWindow = QtGui.QMainWindow()
 ui = qworkspace_ui.Ui_MainWindow()
