9a56ed989509d16090ae9a26cdf2b7a80994b0b0
Ivailo Monev <xakepa10@gmail.com>
2014-08-14 17:22:15 +0300

    version bump

--- src/qworkspace/main.py
+++ src/qworkspace/main.py
@@ -5,7 +5,7 @@ from PyQt4 import QtCore, QtGui
 import sys, os, gc, libworkspace, libmisc
 
 # prepare for lift-off
-app_version = "0.9.23 (d1e5f58)"
+app_version = "0.9.23 (21aee17)"
 app = QtGui.QApplication(sys.argv)
 MainWindow = QtGui.QMainWindow()
 ui = qworkspace_ui.Ui_MainWindow()
