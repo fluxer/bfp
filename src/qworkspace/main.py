ec9010f29cb5a779d91b8618a9a2b99ba6e4b8f7
Ivailo Monev <xakepa10@gmail.com>
2014-08-18 22:25:23 +0300

    removed settings toolbox from main UI

--- src/qworkspace/main.py
+++ src/qworkspace/main.py
@@ -5,7 +5,7 @@ from PyQt4 import QtCore, QtGui
 import sys, os, gc, libworkspace, libmisc
 
 # prepare for lift-off
-app_version = "0.9.27 (46f0784)"
+app_version = "0.9.27 (536a27c)"
 app = QtGui.QApplication(sys.argv)
 MainWindow = QtGui.QMainWindow()
 ui = qworkspace_ui.Ui_MainWindow()
