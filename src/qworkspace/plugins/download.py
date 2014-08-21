d5dd073f51d7cf1d3b70662b347ede82826884f2
Ivailo Monev <xakepa10@gmail.com>
2014-08-21 15:40:42 +0300

    somewhat working download plugin

--- src/qworkspace/plugins/download.py
+++ src/qworkspace/plugins/download.py
@@ -15,7 +15,6 @@ class Widget(QtGui.QWidget):
 
         self.addButton = QtGui.QPushButton(general.get_icon('list-add'), '')
         self.addButton.clicked.connect(self.download_add)
-        self.removeButton = QtGui.QPushButton(general.get_icon('list-remove'), '')
         self.startButton = QtGui.QPushButton(general.get_icon('media-playback-start'), '')
         self.stopButton = QtGui.QPushButton(general.get_icon('process-stop'), '')
         self.secondLayout = QtGui.QHBoxLayout()
@@ -24,17 +23,15 @@ class Widget(QtGui.QWidget):
         self.secondLayout.addWidget(self.startButton)
         self.secondLayout.addWidget(self.stopButton)
         self.mainLayout = QtGui.QGridLayout()
-        self.downloadView = QtGui.QTableWidget()
-        self.downloadView.setColumnCount(3)
-        self.downloadView.setHorizontalHeaderLabels(('#', 'URL', 'Complete'))
+        self.downloadLabel = QtGui.QLabel()
+        self.progressBar = QtGui.QProgressBar()
         self.mainLayout.addLayout(self.secondLayout, 0, 0)
         self.mainLayout.addWidget(self.downloadView)
+        self.mainLayout.addWidget(self.progressBar)
         self.setLayout(self.mainLayout)
 
         self.nam = QtNetwork.QNetworkAccessManager()
         self.download_path = QtCore.QDir.homePath()
-
-        print self.spath
         if self.spath:
             self.download(spath)
 
@@ -44,16 +41,19 @@ class Widget(QtGui.QWidget):
         if surl:
             self.download(surl)
 
-    def download_remove(self):
-        pass
-
     def download(self, surl):
+        self.download_counts+=1
+        number = self.download_counts
         request = QtNetwork.QNetworkRequest(QtCore.QUrl(surl))
         reply = self.nam.get(request)
-        reply.readyRead.connect(lambda reply=reply: self.download_start(reply))
-        reply.finished.connect(lambda reply=reply: self.download_finished(reply))
+        reply.downloadProgress.connect(self.download_progress)
+        reply.readyRead.connect(lambda: self.download_read(reply))
+        reply.finished.connect(lambda: self.download_finished(reply))
 
-    def download_start(self, reply):
+    def download_progress(self, ireceived, itotal):
+        print(ireceived, itotal)
+
+    def download_read(self, reply):
         surl = str(reply.url().toString())
         sfile = self.download_path + '/' + os.path.basename(surl)
         misc.file_write(sfile, reply.readAll(), 'a')
