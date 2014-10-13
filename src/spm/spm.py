4742b9900dfb39fa87feb7565e65b0e8ebd2f1b0
Ivailo Monev <xakepa10@gmail.com>
2014-10-13 19:59:14 +0000

    spm-gui desktop file comment correction

--- src/spm/spm.py
+++ src/spm/spm.py
@@ -16,7 +16,7 @@ else:
     import configparser
     from urllib.error import HTTPError
 
-app_version = "1.1.0 (d71dbe9)"
+app_version = "1.1.0 (d25e950)"
 
 try:
     import libmessage
--- src/spm/tools.py
+++ src/spm/tools.py
@@ -29,7 +29,7 @@ database = libpackage.Database()
 import libspm
 
 
-app_version = "1.1.0 (d71dbe9)"
+app_version = "1.1.0 (d25e950)"
 
 class Check(object):
     ''' Check runtime dependencies of local targets '''
