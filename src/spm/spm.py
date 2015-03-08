2d562bcf3b274d5f153ca16b9f2489429bd1de17
Ivailo Monev <xakepa10@gmail.com>
2015-03-08 17:21:48 +0000

    update TODO

--- src/spm/spm.py
+++ src/spm/spm.py
@@ -19,7 +19,7 @@ else:
     import configparser
     from urllib.error import HTTPError
 
-app_version = "1.6.0 (b493150)"
+app_version = "1.6.0 (00a8e45)"
 
 try:
     import libmessage
--- src/spm/tools.py
+++ src/spm/tools.py
@@ -32,7 +32,7 @@ database = libpackage.Database()
 import libspm
 
 
-app_version = "1.6.0 (b493150)"
+app_version = "1.6.0 (00a8e45)"
 
 class Check(object):
     ''' Check runtime dependencies of local targets '''
