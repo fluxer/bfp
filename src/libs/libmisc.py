90430bc96ce96881adfb84e8ce0b85d6338b34d6
Ivailo Monev <xakepa10@gmail.com>
2015-04-15 23:36:40 +0000

    cleanup libmisc.Misc.url_normalize()

--- src/libs/libmisc.py
+++ src/libs/libmisc.py
@@ -1,5 +1,4 @@
 #!/bin/python2
-# -*- coding: utf-8 -*-
 
 '''
 A module to handle various tasks fuzz free.
@@ -470,120 +469,10 @@ class Misc(object):
             self.typecheck(surl, (types.StringTypes))
             self.typecheck(basename, (types.BooleanType))
 
-        # http://www.w3schools.com/tags/ref_urlencode.asp
-        # FIXME: incomplete URL normalization table
-        dspecials = {'%20': ' ',
-                     '%21': '!',
-                     '%22': '"',
-                     '%23': '#',
-                     '%24': '$',
-                     '%25': '%',
-                     '%26': '&',
-                     '%27': "'",
-                     '%28': '(',
-                     '%29': ')',
-                     '%2A': '*',
-                     '%2B': '+',
-                     '%2C': ',',
-                     '%2D': '-',
-                     '%2E': '.',
-                     '%2F': '/',
-                     '%30': '0',
-                     '%31': '1',
-                     '%32': '2',
-                     '%33': '3',
-                     '%34': '4',
-                     '%35': '5',
-                     '%36': '6',
-                     '%37': '7',
-                     '%38': '8',
-                     '%39': '9',
-                     '%3A': ':',
-                     '%3B': ';',
-                     '%3C': '<',
-                     '%3D': '=',
-                     '%3E': '>',
-                     '%3F': '?',
-                     '%40': '@',
-                     '%41': 'A',
-                     '%42': 'B',
-                     '%43': 'C',
-                     '%44': 'D',
-                     '%45': 'E',
-                     '%46': 'F',
-                     '%47': 'G',
-                     '%48': 'H',
-                     '%49': 'I',
-                     '%4A': 'J',
-                     '%4B': 'K',
-                     '%4C': 'L',
-                     '%4D': 'M',
-                     '%4E': 'N',
-                     '%4F': 'O',
-                     '%50': 'P',
-                     '%51': 'Q',
-                     '%52': 'R',
-                     '%53': 'S',
-                     '%54': 'T',
-                     '%55': 'U',
-                     '%56': 'V',
-                     '%57': 'W',
-                     '%58': 'X',
-                     '%59': 'Y',
-                     '%5A': 'Z',
-                     '%5B': '[',
-                     '%5C': '\\',
-                     '%5D': ']',
-                     '%5E': '^',
-                     '%5F': '_',
-                     '%60': '`',
-                     '%61': 'a',
-                     '%62': 'b',
-                     '%63': 'c',
-                     '%64': 'd',
-                     '%65': 'e',
-                     '%66': 'f',
-                     '%67': 'g',
-                     '%68': 'h',
-                     '%69': 'i',
-                     '%6A': 'j',
-                     '%6B': 'k',
-                     '%6C': 'l',
-                     '%6D': 'm',
-                     '%6E': 'n',
-                     '%6F': 'o',
-                     '%70': 'p',
-                     '%71': 'q',
-                     '%72': 'r',
-                     '%73': 's',
-                     '%74': 't',
-                     '%75': 'u',
-                     '%76': 'v',
-                     '%78': 'w',
-                     '%79': 'x',
-                     '%7A': 'z',
-                     '%7B': '{',
-                     '%7C': '|',
-                     '%7D': '}',
-                     '%7E': '~',
-                     '%7F': ' ', # TODO: is that supposed to be tab?
-                     '%80': '`', '%E2%82%AC': '`',
-                     '%81': '',
-                     '%82': '‚', '%E2%80%9A': '‚',
-                     '%83': 'ƒ', '%C6%92': 'ƒ',
-                     '%84': '„', '%E2%80%9E': '„',
-                     '%85': '…', '%E2%80%A6': '…',
-                     '%86': '†', '%E2%80%A0': '†',
-                     '%87': '‡', '%E2%80%A1': '‡',
-                     '%88': 'ˆ', '%CB%86': 'ˆ',
-                     '%89': '‰', '%E2%80%B0': '‰',
-                     }
-        sresult = urlparse(surl).path
-        for schar in dspecials:
-            sresult = sresult.replace(schar, dspecials[schar])
+        path = urlparse(surl).path
         if basename:
-            return os.path.basename(sresult)
-        return sresult
+            return os.path.basename(path)
+        return path
 
 
     def url_ping(self, surl='http://www.google.com', lmirrors=None, ssuffix=''):
