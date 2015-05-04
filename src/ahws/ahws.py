ab7d4980d8deb961931274367df49907aa791781
Ivailo Monev <xakepa10@gmail.com>
2015-05-04 18:25:59 +0000

    ahws refactoring

--- src/ahws/ahws.py
+++ src/ahws/ahws.py
@@ -5,7 +5,7 @@ message = libmessage.Message()
 message.DEBUG = True
 misc = libmisc.Misc()
 
-app_version = "1.7.6 (83ae7f1)"
+app_version = "1.7.6 (9977390)"
 
 class Device(object):
     def __init__(self):
@@ -13,81 +13,103 @@ class Device(object):
         if not self.udev:
             message.sub_critical('Can not get udev context')
             sys.exit(1)
-        self.CONFDIR = '/etc/ahws.d'
+        self.HANDLERS_DIR = '/etc/ahws.d'
+        self.SUBSYSTEMS = ['pci', 'usb', 'video4linux', 'sound', 'snd', 'printer', 'drm']
+
+    def Properties(self, dev):
+        DEVNAME = libudev.udev_device_get_property_value(dev, 'DEVNAME')
+        if not DEVNAME:
+            DEVNAME = libudev.udev_device_get_property_value(dev,'KERNEL')
+        if not DEVNAME:
+            DEVNAME = 'Unknown'
+
+        # ieee1394_id ?
+        PRODUCT = libudev.udev_device_get_property_value(dev, 'ID_MODEL_ID')
+        if not PRODUCT:
+            PRODUCT = libudev.udev_device_get_sysattr_value(dev,'idProduct')
+        if not PRODUCT:
+            PRODUCT = libudev.udev_device_get_sysattr_value(dev,'device')
+
+        VENDOR = libudev.udev_device_get_property_value(dev, 'ID_VENDOR_ID')
+        if not VENDOR:
+            VENDOR = libudev.udev_device_get_sysattr_value(dev,'idVendor')
+        if not VENDOR:
+            VENDOR = libudev.udev_device_get_sysattr_value(dev,'vendor')
+
+        SERIAL = libudev.udev_device_get_property_value(dev, 'ID_SERIAL')
+        if not SERIAL:
+            libudev.udev_device_get_sysattr_value(dev, 'serial')
+        if not SERIAL:
+            libudev.udev_device_get_sysattr_value(dev, 'product')
+
+        return DEVNAME, PRODUCT, VENDOR, SERIAL
 
     def Initialize(self):
         ''' Handle all current devices '''
         enumerate = libudev.udev_enumerate_new(self.udev);
         try:
-            # libudev.udev_enumerate_add_match_subsystem(enumerate, "pci");
+            for subsystem in self.SUBSYSTEMS:
+                libudev.udev_enumerate_add_match_subsystem(enumerate, subsystem)
             libudev.udev_enumerate_scan_devices(enumerate);
             entry = libudev.udev_enumerate_get_list_entry(enumerate)
             while entry:
                 devname = libudev.udev_list_entry_get_name(entry)
                 dev = libudev.udev_device_new_from_syspath(self.udev, devname)
-                DEVNAME = libudev.udev_device_get_property_value(dev, 'DEVNAME')
-                ID_MODEL_ID = libudev.udev_device_get_property_value(dev, 'ID_MODEL_ID')
-                ID_VENDOR_ID = libudev.udev_device_get_property_value(dev, 'ID_VENDOR_ID')
-                ID_SERIAL = libudev.udev_device_get_property_value(dev, 'ID_SERIAL')
-
-                # print(libudev.udev_device_get_sysattr_value(dev,"idVendor"))
-                # print(libudev.udev_device_get_sysattr_value(dev, "idProduct"))
-
-                self.Online(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL)
+                self.Online(self.Properties(dev))
                 libudev.udev_device_unref(dev)
                 entry = libudev.udev_list_entry_get_next(entry)
         finally:
             libudev.udev_enumerate_unref(enumerate);
 
-    def Handle(self, DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL, action):
-        handle = '%s/%s_%s' % (self.CONFDIR, ID_MODEL_ID, ID_VENDOR_ID)
-        if not DEVNAME:
-            return
-        if not ID_MODEL_ID or not ID_VENDOR_ID:
+    def Handle(self, properties, action):
+        DEVNAME, MODEL, VENDOR, SERIAL = properties
+        handle = '%s/%s_%s' % (self.HANDLERS_DIR, VENDOR, MODEL)
+        if not MODEL or not VENDOR:
             message.sub_warning('Model or vendor ID missing for', \
-                '%s (%s, %s)' % (DEVNAME, ID_MODEL_ID, ID_VENDOR_ID))
+                '%s (%s, %s)' % (DEVNAME, VENDOR, MODEL))
             return
         elif os.path.isfile(handle):
             message.sub_info('Handling event for', \
-                '%s (%s, %s)' % (ID_SERIAL, ID_MODEL_ID, ID_VENDOR_ID))
+                '%s (%s, %s)' % (SERIAL, VENDOR, MODEL))
             try:
                 misc.system_command((handle, action))
             except Exception as detail:
                 message.sub_critical(str(detail))
         else:
             message.sub_debug('No handle for', \
-                '%s (%s, %s)' % (ID_SERIAL, ID_MODEL_ID, ID_VENDOR_ID))
+                '%s (%s, %s)' % (SERIAL, VENDOR, MODEL))
 
-    def Add(self, DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL):
+    def Add(self, properties):
         ''' Emit that device add action happend '''
-        message.sub_info('Added', DEVNAME)
-        self.Handle(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL, 'added')
+        message.sub_info('Added', properties[0])
+        self.Handle(properties, 'added')
 
-    def Remove(self, DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL):
+    def Remove(self, properties):
         ''' Emit that device remove action happend '''
-        message.sub_info('Removed', DEVNAME)
-        self.Handle(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL, 'removed')
+        message.sub_info('Removed', properties[0])
+        self.Handle(properties, 'removed')
 
-    def Change(self, DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL):
+    def Change(self, properties):
         ''' Emit that device change action happend '''
-        message.sub_info('Changed', DEVNAME)
-        self.Handle(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL, 'changed')
+        message.sub_info('Changed', properties[0])
+        self.Handle(properties, 'changed')
 
-    def Online(self, DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL):
+    def Online(self, properties):
         ''' Emit that device online action happend '''
-        message.sub_info('Onlined', DEVNAME)
-        self.Handle(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL, 'onlined')
+        message.sub_info('Onlined', properties[0])
+        self.Handle(properties, 'onlined')
 
-    def Offline(self, DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL):
+    def Offline(self, properties):
         ''' Emit that device offline action happend '''
-        message.sub_info('Offlined', DEVNAME)
-        self.Handle(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL, 'offlined')
+        message.sub_info('Offlined', properties[0])
+        self.Handle(properties, 'offlined')
 
     def Daemon(self):
         ''' Daemon that monitors events '''
         try:
             monitor = libudev.udev_monitor_new_from_netlink(self.udev, 'udev')
-            # libudev.udev_monitor_filter_add_match_subsystem_devtype(monitor, "pci", None)
+            for subsystem in self.SUBSYSTEMS:
+                libudev.udev_monitor_filter_add_match_subsystem_devtype(monitor, subsystem, None)
             libudev.udev_monitor_enable_receiving(monitor)
             while True:
                 if monitor:
@@ -95,22 +117,18 @@ class Device(object):
                 if monitor and fd:
                     dev = libudev.udev_monitor_receive_device(monitor)
                     if dev:
-                        DEVNAME = libudev.udev_device_get_property_value(dev, 'DEVNAME')
-                        ID_MODEL_ID = libudev.udev_device_get_property_value(dev, 'ID_MODEL_ID')
-                        ID_VENDOR_ID = libudev.udev_device_get_property_value(dev, 'ID_VENDOR_ID')
-                        ID_SERIAL = libudev.udev_device_get_property_value(dev, 'ID_SERIAL')
                         action = libudev.udev_device_get_action(dev)
+                        if action == 'add':
+                            self.Add(self.Properties(dev))
+                        elif action == 'remove':
+                            self.Remove(self.Properties(dev))
+                        elif action == 'change':
+                            self.Change(self.Properties(dev))
+                        elif action == 'online':
+                            self.Online(self.Properties(dev))
+                        elif action == 'offline':
+                            self.Offline(self.Properties(dev))
                         libudev.udev_device_unref(dev)
-                        if action == 'add' and DEVNAME:
-                            self.Add(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL)
-                        elif action == 'remove' and DEVNAME:
-                            self.Remove(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL)
-                        elif action == 'change' and DEVNAME:
-                            self.Change(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL)
-                        elif action == 'online' and DEVNAME:
-                            self.Online(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL)
-                        elif action == 'offline' and DEVNAME:
-                            self.Offline(DEVNAME, ID_MODEL_ID, ID_VENDOR_ID, ID_SERIAL)
                     else:
                         time.sleep(1)
         except Exception as detail:
