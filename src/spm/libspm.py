fluxer/bfp

commit 784977e265b71c5d661df3ef023e3c6d9eee73a6

close opened objects on failures too
for consistent code-style try/finally code blocks are used altough
I'm aware of the with statement.

src/spm/libspm.py
@@ -103,13 +103,11 @@ if not os.path.isfile(REPOSITORIES_CONF):
     REPOSITORIES = ['https://bitbucket.org/smil3y/mini.git']
 else:
     REPOSITORIES = []
-    repositories_conf = open(REPOSITORIES_CONF, 'r')
-    for line in repositories_conf.readlines():
+    for line in misc.file_readlines(REPOSITORIES_CONF):
         line = line.strip()
         if line.startswith(('http://', 'https://', 'ftp://', 'ftps://', \
             'git://', 'ssh://', 'rsync://')) or os.path.exists(line):
             REPOSITORIES.append(line)
-    repositories_conf.close()
 
     if not REPOSITORIES:
         message.critical(_('Repositories configuration file is empty'))
@@ -122,12 +120,10 @@ if not os.path.isfile(MIRRORS_CONF):
     MIRRORS = ['http://distfiles.gentoo.org/distfiles']
 else:
     MIRRORS = []
-    mirrors_conf = open(MIRRORS_CONF, 'r')
-    for line in mirrors_conf.readlines():
+    for line in misc.file_readlines(MIRRORS_CONF):
         line = line.strip()
         if line.startswith(('http://', 'https://', 'ftp://', 'ftps://')):
             MIRRORS.append(line)
-    mirrors_conf.close()
 
     if not MIRRORS and MIRROR:
         message.critical(_('Mirrors configuration file is empty'))
@@ -140,12 +136,10 @@ if not os.path.isfile(KEYSERVERS_CONF):
     KEYSERVERS = ['pool.sks-keyservers.net']
 else:
     KEYSERVERS = []
-    mirrors_conf = open(KEYSERVERS_CONF, 'r')
-    for line in mirrors_conf.readlines():
+    for line in misc.file_readlines(KEYSERVERS_CONF):
         line = line.strip()
         if line:
             KEYSERVERS.append(line)
-    mirrors_conf.close()
 
     if not KEYSERVERS and VERIFY:
         message.critical(_('PGP keys servers configuration file is empty'))
@@ -1096,13 +1090,12 @@ class Source(object):
 
         message.sub_info(_('Assembling metadata'))
         misc.dir_create(os.path.join(self.install_dir, 'var/local/spm', self.target_name))
-        metadata = open(os.path.join(self.install_dir, self.target_metadata), 'w')
-        metadata.write('version=%s\n' % self.target_version)
-        metadata.write('release=%s\n' % self.target_release)
-        metadata.write('description=%s\n' % self.target_description)
-        metadata.write('depends=%s\n' % misc.string_convert(self.target_depends))
-        metadata.write('size=%d\n' % misc.dir_size(self.install_dir))
-        metadata.close()
+        data = 'version=%s\n' % self.target_version
+        data += 'release=%s\n' % self.target_release
+        data += 'description=%s\n' % self.target_description
+        data += 'depends=%s\n' % misc.string_convert(self.target_depends)
+        data += 'size=%d\n' % misc.dir_size(self.install_dir)
+        misc.file_write(os.path.join(self.install_dir, self.target_metadata), data)
 
         message.sub_info(_('Assembling footprint'))
         # due to creations and deletions of files, when byte-compiling

src/libs/libmisc.py
@@ -194,8 +194,10 @@ class Misc(object):
             self.typecheck(sfile, (types.StringTypes))
 
         rfile = open(sfile, 'rb')
-        content = rfile.read()
-        rfile.close()
+        try:
+            content = rfile.read()
+        finally:
+            rfile.close()
         return self.string_encode(content)
 
     def file_read_nonblock(self, sfile, ibuffer=1024):
@@ -205,8 +207,10 @@ class Misc(object):
             self.typecheck(ibuffer, (types.IntType))
 
         fd = os.open(sfile, os.O_NONBLOCK)
-        content = os.read(fd, ibuffer)
-        os.close(fd)
+        try:
+            content = os.read(fd, ibuffer)
+        finally:
+            os.close(fd)
         return content
 
     def file_readlines(self, sfile):
@@ -215,8 +219,10 @@ class Misc(object):
             self.typecheck(sfile, (types.StringTypes))
 
         rfile = open(sfile, 'rb')
-        content = rfile.read().splitlines()
-        rfile.close()
+        try:
+            content = rfile.read().splitlines()
+        finally:
+            rfile.close()
         return self.string_encode(content)
 
     def file_write(self, sfile, content, mode='w'):
@@ -228,8 +234,10 @@ class Misc(object):
 
         self.dir_create(os.path.dirname(sfile))
         wfile = open(sfile, mode)
-        wfile.write(content)
-        wfile.close()
+        try:
+            wfile.write(content)
+        finally:
+            wfile.close()
 
     def file_write_nonblock(self, sfile, content):
         ''' Write data to file non-blocking (overwrites) '''
@@ -239,8 +247,10 @@ class Misc(object):
 
         self.dir_create(os.path.dirname(sfile))
         fd = os.open(sfile, os.O_NONBLOCK | os.O_WRONLY)
-        os.write(fd, content)
-        os.close(fd)
+        try:
+            os.write(fd, content)
+        finally:
+            os.close(fd)
 
     def file_search(self, string, sfile, exact=False, escape=True):
         ''' Search for string in file '''
@@ -659,14 +669,18 @@ class Misc(object):
 
         if sfile.endswith(('tar.bz2', '.tar.gz')):
             tarf = tarfile.open(sfile, 'w:' + self.file_extension(sfile))
-            for item in lpaths:
-                tarf.add(item, item.lstrip(strip))
-            tarf.close()
+            try:
+                for item in lpaths:
+                    tarf.add(item, item.lstrip(strip))
+            finally:
+                tarf.close()
         elif sfile.endswith('.zip'):
             zipf = zipfile.ZipFile(sfile, mode='w')
-            for item in lpaths:
-                zipf.write(item, item.lstrip(strip))
-            zipf.close()
+            try:
+                for item in lpaths:
+                    zipf.write(item, item.lstrip(strip))
+            finally:
+                zipf.close()
         elif sfile.endswith(('.xz', '.lzma')):
             tar = self.whereis('bsdtar', fallback=False)
             if not tar:
@@ -734,10 +748,12 @@ class Misc(object):
         smime = self.file_mime(sfile, True)
         if tarfile.is_tarfile(sfile):
             tfile = tarfile.open(sfile)
-            for member in tfile.getmembers():
-                if not member.isdir():
-                    content.append(member.name)
-            tfile.close()
+            try:
+                for member in tfile.getmembers():
+                    if not member.isdir():
+                        content.append(member.name)
+            finally:
+                tfile.close()
         elif zipfile.is_zipfile(sfile):
             zfile = zipfile.ZipFile(sfile)
             content = zfile.namelist()
@@ -764,11 +780,13 @@ class Misc(object):
 
         sizes = []
         tar = tarfile.open(star, 'r')
-        for i in tar.getmembers():
-            for sfile in lpaths:
-                if i.name == sfile:
-                    sizes.append(i.size)
-        tar.close()
+        try:
+            for i in tar.getmembers():
+                for sfile in lpaths:
+                    if i.name == sfile:
+                        sizes.append(i.size)
+        finally:
+            tar.close()
         return sizes
 
     def archive_content(self, star, lpaths):

src/spm/spm.py
@@ -19,7 +19,7 @@ else:
     import configparser
     from urllib.error import HTTPError
 
-app_version = "1.7.6 (3d8d0ff)"
+app_version = "1.7.6 (e7290fc)"
 
 try:
     import libmessage

src/spm/tools.py
@@ -36,7 +36,7 @@ database = libpackage.Database()
 import libspm
 misc.GPG_DIR = libspm.GPG_DIR
 
-app_version = "1.7.6 (3d8d0ff)"
+app_version = "1.7.6 (e7290fc)"
 
 class Check(object):
     ''' Check runtime dependencies of local targets '''
@@ -640,6 +640,7 @@ class Pkg(object):
         """Search the Git interface on archlinux.org."""
         for d in self.GIT_DIRS:
             url = self.GIT_URL + d % pkgname
+            f = None
             try:
                 f = urlopen(url)
                 for line in f:
@@ -649,11 +650,14 @@ class Pkg(object):
                         name = m.group(2).decode()
                         if name[:2] != '..':
                             yield self.GIT_URL + href, name
-                f.close()
-                return
             except HTTPError as e:
                 if e.code != 404:
+                    if f:
+                        f.close()
                     raise
+            finally:
+                if f:
+                    f.close()
 
     def main(self):
         not_found = []

commit 351d769d1e6e2063807cfd167e19ddebfa920551

fix Python 3000 support regression since 784977e2

src/spm/libspm.py
@@ -106,7 +106,7 @@ if not os.path.isfile(REPOSITORIES_CONF):
 else:
     REPOSITORIES = []
     for line in misc.file_readlines(REPOSITORIES_CONF):
-        line = line.strip()
+        line = misc.string_encode(line.strip())
         if line.startswith(('http://', 'https://', 'ftp://', 'ftps://', \
             'git://', 'ssh://', 'rsync://')) or os.path.exists(line):
             REPOSITORIES.append(line)
@@ -123,7 +123,7 @@ if not os.path.isfile(MIRRORS_CONF):
 else:
     MIRRORS = []
     for line in misc.file_readlines(MIRRORS_CONF):
-        line = line.strip()
+        line = misc.string_encode(line.strip())
         if line.startswith(('http://', 'https://', 'ftp://', 'ftps://')):
             MIRRORS.append(line)
 
@@ -139,7 +139,7 @@ if not os.path.isfile(KEYSERVERS_CONF):
 else:
     KEYSERVERS = []
     for line in misc.file_readlines(KEYSERVERS_CONF):
-        line = line.strip()
+        line = misc.string_encode(line.strip())
         if line:
             KEYSERVERS.append(line)
 