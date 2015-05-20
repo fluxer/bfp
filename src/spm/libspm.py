54b76b12a8670dee804f677da52604a2ed720a15
Ivailo Monev <xakepa10@gmail.com>
2015-05-20 11:33:52 +0000

    do not parse the repositories config on repo prune

--- src/spm/libspm.py
+++ src/spm/libspm.py
@@ -401,12 +401,8 @@ class Repo(object):
 
         for sdir in os.listdir(rdir):
             valid = False
-            for line in misc.file_readlines('/etc/spm/repositories.conf'):
-                line = misc.string_encode(line.strip())
-                if not line or line.startswith('#'):
-                    continue
-
-                if os.path.basename(line) == sdir:
+            for repo in REPOSITORIES:
+                if os.path.basename(repo) == sdir:
                     valid = True
 
             if not valid:
