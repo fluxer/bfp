fce00a374500a7e06bf95625dac10753c3d98195
Ivailo Monev <xakepa10@gmail.com>
2015-05-24 22:11:44 +0000

    another day - another weird shabang to handle (hint: docbook-xsl)

--- src/spm/libspm.py
+++ src/spm/libspm.py
@@ -1025,7 +1025,7 @@ class Source(object):
                 # parse the shebang and split it to 2 groups:
                 # 1. full match, used to replace it with something that will work
                 # 2. base of the interpreter (e.g. bash), used to find match in the target or host
-                omatch = misc.file_search('(^#!.*(?: |\\t|/)((?:' + bang_regexp + ')(.* |.*\\t)?))(?:.*\\s)', \
+                omatch = misc.file_search('(^#!.*(?: |\\t|/)((?:' + bang_regexp + ')(?:[^\\s]+)?)(?:.*\\s))', \
                     sfile, exact=False, escape=False)
                 if omatch:
                     sfull = omatch[0][0].strip()
