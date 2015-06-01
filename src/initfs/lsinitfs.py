c299f32a7ff9fb59903268c96667711e5fa55cd9
Ivailo Monev <xakepa10@gmail.com>
2015-06-01 01:21:12 +0100

    fixup spmd cold target

--- src/initfs/lsinitfs.py
+++ src/initfs/lsinitfs.py
@@ -2,7 +2,7 @@
 
 import sys, argparse, tempfile, subprocess, shutil, os
 
-app_version = "1.7.6 (7fb30b9)"
+app_version = "1.7.6 (ad0515b)"
 
 tmpdir = None
 keep = False
--- src/spmd/client.py
+++ src/spmd/client.py
@@ -35,7 +35,7 @@ def dbus_call(method, args=None):
     else:
         message.sub_critical(str(bus.lastError().message()))
 
-app_version = "1.7.6 (20f6a5b)"
+app_version = "1.7.6 (ad0515b)"
 
 try:
     parser = argparse.ArgumentParser(prog='spmctl', \
