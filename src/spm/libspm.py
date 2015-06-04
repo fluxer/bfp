2d423a0abbb4901f9ecf67bc7653439a43f8aa5d
Ivailo Monev <xakepa10@gmail.com>
2015-06-04 16:08:43 +0000

    oops, do not always export optionals as yes

--- src/spm/libspm.py
+++ src/spm/libspm.py
@@ -565,7 +565,7 @@ class Source(object):
                 option = 'yes'
             else:
                 message.sub_debug(_('Disabling optional'), target)
-            os.putenv('OPTIONAL_%s' % target.upper(), 'yes')
+            os.putenv('OPTIONAL_%s' % target.upper(), option)
 
         misc.dir_create(self.source_dir)
         misc.dir_create(self.install_dir)
