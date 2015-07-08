6d6fecc96c850a407e97ff91c639b55847e68900
Ivailo Monev <xakepa10@gmail.com>
2015-07-08 09:12:05 +0300

    improve example plugin

--- doc/example-plugin.py
+++ doc/example-plugin.py
@@ -6,7 +6,8 @@ _ = gettext.translation('spm', fallback=True).gettext
 import libmessage
 message = libmessage.Message()
 
-
+# in case someone wants to use the pluging without setting up a parser a simple
+# class that gets the job done is a nice to have
 class Hello(object):
     def __init__(self, msg='Hello World from SPM Tools!'):
         self.msg = msg
@@ -14,6 +15,8 @@ class Hello(object):
     def main(self):
         print(self.msg)
 
+
+# that's what spm-tools is dealing with at runtime
 class Main(object):
     def __init__(self, subparsers):
         hello_parser = subparsers.add_parser('hello')
@@ -28,3 +31,15 @@ class Main(object):
             message.info(_('Poking the world...'))
             m = Hello(ARGS.msg)
             m.main()
+
+
+# finally, standalone usage. without Hello() anything that wants to access the
+# pluging will have to write similar code
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog='spm-tools', \
+        description='Source Package Manager Tools Hello World plugin')
+    subparsers = parser.add_subparsers(dest='mode')
+    m = Main(subparsers)
+    ARGS = parser.parse_args()
+    m.run(ARGS)
