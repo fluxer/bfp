adef8fff981bf4c0f1ce392d10913dd3a69d5a28
Ivailo Monev <xakepa10@gmail.com>
2015-07-22 04:33:33 +0300

    extend the benchmark to local targets

--- src/libs/benchmark.py
+++ src/libs/benchmark.py
@@ -23,7 +23,21 @@ def create_remote(name, version, release, description, \
     srcbuild.write('\nbackup=(%s)' % backup)
     srcbuild.close()
 
-def benchmark(t):
+def create_local(name, version, release, description, depends='', \
+        optdepends='', backup='', footprint=''):
+    data = {}
+    data['version'] = version
+    data['release'] = release
+    data['description'] = description
+    data['depends'] = str(depends).split()
+    data['optdepends'] = str(optdepends).split()
+    data['backup'] = str(backup).split()
+    data['footprint'] = str(footprint).split()
+    sdir = '%s/%s' % (database.LOCAL_DIR, name)
+    os.makedirs(sdir)
+    misc.json_write('%s/metadata.json' % sdir, data)
+
+def benchmark_remote(t):
     # do not setup database notifier to avoid hitting the system limit
     # which is usually ~8200
     database.NOTIFY = False
@@ -42,7 +56,28 @@ def benchmark(t):
         # print('cleaning up ...')
         misc.dir_remove(database.ROOT_DIR)
 
+def benchmark_local(t):
+    # do not setup database notifier to avoid hitting the system limit
+    # which is usually ~8200
+    database.NOTIFY = False
+    database.ROOT_DIR = tempfile.mkdtemp()
+    database.CACHE_DIR = '%s/var/cache/spm' % database.ROOT_DIR
+    database.BUILD_DIR = '%s/var/tmp/spm' % database.ROOT_DIR
+    database.LOCAL_DIR = '%s/var/local/spm' % database.ROOT_DIR
+    try:
+        # print('preparing for benchmark...')
+        for r in range(5000):
+            create_local(r, r, r, r, r, r, r, r)
+        # print('running benchmark %d...' % t)
+        print(timeit.timeit(database.local_all, number=1))
+        database.LOCAL_CACHE = {}
+    finally:
+        # print('cleaning up ...')
+        misc.dir_remove(database.ROOT_DIR)
+
 for t in range(3):
     # best of three
-    benchmark(t)
-
+    benchmark_remote(t)
+for t in range(3):
+    # best of three
+    benchmark_local(t)
