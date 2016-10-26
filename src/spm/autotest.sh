36d3bf3efde7bd4509d6a83346a06a9708d12d15
Ivailo Monev <xakepa10@laimg.moc>
2016-10-26 05:52:44 +0000

    ignore ownership issues when running auto tests
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- src/spm/autotest.sh
+++ src/spm/autotest.sh
@@ -16,7 +16,7 @@ rootdir="$curdir/root-$1"
 cachedir="$rootdir/cache"
 builddir="$rootdir/build"
 gpgdir="$rootdir/gpg"
-spmargs="--root $rootdir --cache $cachedir --build $builddir --gpg $gpgdir --missing=True --notify=True --scripts=False --debug"
+spmargs="--root $rootdir --cache $cachedir --build $builddir --gpg $gpgdir --missing=True --notify=True --scripts=False --ownership=True --debug"
 spmtargs="--root $rootdir --debug"
 statefile="$rootdir/testrun-$1"
 
